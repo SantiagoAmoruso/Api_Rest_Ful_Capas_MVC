d14371dbe2d179c6fb74acb60f0a0a348d6bf431
Amoruso Santiago <amoruso.santiago@tecnica7.edu.ar>
2025-10-06 20:27:02 -0300

    SRC

---
+++ src/Routes/users.routes.js
@@ -0,0 +1,85 @@
+import { Router } from "express";
+
+import pool from '../../config/conexion.js'
+
+const router = Router()
+
+router.get('/users', async(req,res) =>{
+    const sql = 'select * from users'
+    try{
+        const connection = await pool.getConnection()
+        const [rows] = await connection.query(sql)
+        connection.release()
+        res.json(rows)
+    }catch(error){
+        res.status(500).send('error con la consulta')
+    }
+    
+})
+
+router.get('/users/:id', async(req, res) => {
+    const id = req.params.id
+    const sql = 'select * from users where id_users = ?'  
+   
+    try{
+        const connection = await pool.getConnection()
+        const [rows] = await connection.query(sql, [id])
+        connection.release()
+        rows[0] ? res.json(rows[0]):res.status(404).send('user no existe')
+        console.log(rows[0])
+        
+    }catch(error){
+        res.status(500).send('error con la consulta')
+    }
+})
+
+router.post('/users/body', async(req,res) => {
+    const values = req.body
+    // console.log(values)
+    const sql = 'insert into users SET ?'  
+   try{
+        const connection = await pool.getConnection()
+        const [rows] = await connection.query(sql, [values])
+        connection.release()
+        res.status(500).send('nuevo usuario')
+        console.log(rows)
+        
+    }catch(error){
+        console.log(error)
+        res.status(500).send('error con la consulta')
+    }
+})
+
+router.put('/users/:id', async(req,res) => {
+    const id =req.params.id
+    const new_values = req.body
+    const sql = "update users set ? where id_users = ? "
+    try{
+        const connection = await pool.getConnection()
+        const [rows] = await connection.query(sql, [new_values, id]);
+        connection.release();
+        (rows.affectedRows == 0 )? res.status(404).send('user no existe'):res.send('datos actualizados')
+        
+    }catch(error){
+        //console.log(error)
+        res.status(500).send('error con la consulta'+ error)
+    }
+})
+
+router.delete('/users/:id', async(req,res) => {
+     const id =req.params.id
+     const sql = "delete from users where id_users = ?"
+
+      try{
+        const connection = await pool.getConnection()
+        const [rows] = await connection.query(sql, [id]);
+        connection.release();
+        (rows.affectedRows == 0 )? res.status(404).send('user no existe'):res.send('user eliminado')
+    
+    }catch(error){
+        //console.log(error)
+        res.status(500).send('error con la consulta'+ error)
+    }
+})
+
+export default router
